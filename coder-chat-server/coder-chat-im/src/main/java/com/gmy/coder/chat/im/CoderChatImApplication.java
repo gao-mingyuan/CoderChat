d9ee0668a82b529a2f20c6d686397583cc3f94b7
gaomingyuan <1015580189@qq.com>
2024-11-11 23:23:35 +0800

    feat:网关模块,nacos模块

--- coder-chat-server/coder-chat-im/src/main/java/com/gmy/coder/chat/im/CoderChatImApplication.java
+++ coder-chat-server/coder-chat-im/src/main/java/com/gmy/coder/chat/im/CoderChatImApplication.java
@@ -2,8 +2,9 @@ package com.gmy.coder.chat.im;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gmy.coder.chat.im")
 public class CoderChatImApplication {
 
     public static void main(String[] args) {
