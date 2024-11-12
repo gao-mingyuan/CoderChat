fb0b212e4173caec5d021dbe4de2c4beba3cd3fa
gaomingyuan <1015580189@qq.com>
2024-11-12 20:40:16 +0800

    feat:网关模块,nacos模块

---
+++ coder-chat-server/coder-chat-im/src/test/java/com/gmy/coder/chat/im/CoderChatImApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gmy.coder.chat.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderChatImApplicationTests {
+
+	@Test
+	void contextLoads() {
+        System.out.println(1);
+	}
+
+}
